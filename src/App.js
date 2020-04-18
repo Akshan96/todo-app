bf099f6984b2497aca80edae5f62eaf5c68c224f
akshan96 <akshanpatel1996@gmail.com>
2020-04-18 19:25:48 -0400

    update counter

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
 import React, { Component } from 'react';
-import FirstComponent from './components/learning-examples/FirstComponent'
-import SecondComponent from './components/learning-examples/SecondComponent'
-import ThirdComponent from './components/learning-examples/ThirdComponent'
+//import FirstComponent from './components/learning-examples/FirstComponent'
+//import SecondComponent from './components/learning-examples/SecondComponent'
+//import ThirdComponent from './components/learning-examples/ThirdComponent'
+import Counter from './components/counter/Counter'
+//import TodoApp from './components/todo/TodoApp'
 import './App.css';
- 
+import './bootstrap.css';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        My Hello World
-        <LearningComponents></LearningComponents>
+        {/*<Counter/>*/}
+        <Counter/>
       </div>
     );
   }
 }
 
-
-
-class LearningComponents extends Component {
-  render() {
-    return (
-      <div className="LearningComponents">
-         My Hello World
-         <FirstComponent></FirstComponent>
-         <SecondComponent></SecondComponent>
-         <ThirdComponent></ThirdComponent>
-      </div>
-    );
-  }
-}
+// class LearningComponents extends Component {
+//   render() {
+//     return (
+//       <div className="LearningComponents">
+//          My Hello World
+//          <FirstComponent></FirstComponent>
+//          <SecondComponent></SecondComponent>
+//          <ThirdComponent></ThirdComponent>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
